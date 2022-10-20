8c299c96854858d5aacf91716382b7fb340c4fe7
JasonGH17 <jason.ghantous@outlook.com>
2022-10-20 22:39:54 +0300

    Updated build script

--- build.ps1
+++ build.ps1
@@ -2,5 +2,12 @@ cd ./cig-gui
 pnpm run build
 pnpm run postbuild
 cd ..
-go build ./src
+
+# Build target
+$env:build_os = 'windows'
+
+cd ./src
+go build -ldflags "-X main.build_os=$env:build_os"
+Move-Item -Path "./src.exe" -Destination "../"
+cd ..
 Rename-Item -Path "./src.exe" -NewName "CIG.exe"
